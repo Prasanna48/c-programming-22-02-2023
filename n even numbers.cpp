e6bec41924963c097e8aa8956c07f90613bc9feb
Prasanna48 <125577633+Prasanna48@users.noreply.github.com>
2023-02-22 20:09:08 +0530

    Add files via upload

--- n even numbers.cpp
+++ n even numbers.cpp
@@ -2,6 +2,13 @@
 int main()
 {
 	int n,i,j;
-	printf("")
+	printf("enter a number");
+	scanf("%d",&n);
+	printf("first %d even numbers are:\n",n);
+	for(j=1,i=0;j<=n;i=i+2,j++)
+	{
+		printf("%d",i);
+	}
+	return 0;
 }
 
